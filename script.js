2d8b16a15500dd31f2a0936533a31e1df3081e0a
Diego Roberto <darkm8m@gmail.com>
2024-02-13 22:38:12 -0600

    Primer Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function cambiaMensaje(){
+    document.getElementById("loading-message").innerText = "Te quiero mi lucil muak<3"
+}
+
+setTimeout(cambiaMensaje,2500)
+
+
+setTimeout(function(){
+    alert("resiviste un abrazo")
+},2400)
